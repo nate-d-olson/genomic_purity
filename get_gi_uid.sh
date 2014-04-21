23d1f07ac70c9d072acc0ce319469a9702af4ba3
Nate Olson <nathandavidolson@gmail.com>
2014-04-21 15:31:45 -0400

    script for create a flat file with uid and gi

---
+++ get_gi_uid.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -       
+#title           :get_gi_uid.sh
+#description     :Script for generating a organism name,uid, gi flat file
+#author		 :Nate Olson
+#date            :2014-04-21
+#version         :0.1   
+#usage		 :bash get_gi_uid.sh
+#notes           :Need to make sure the DB_DIR points to the location for a local GenBank database
+#==============================================================================
+
+
+# variables
+DB_DIR="/Users/nolson/Documents/ncbi_bac"
+echo "DIR,UID,FASTA,GI" > /Users/nolson/Documents/mirror/purity_study/pathoscope_pure_id/gi_uid_db.csv
+DB_OUT=/Users/nolson/Documents/mirror/purity_study/pathoscope_pure_id/gi_uid_db.csv
+
+#functions
+function parse_directory_name(){
+	# take the folder name as input and return the organism name and uid number
+	ORG_NAME_UID=$(echo $1 | sed 's|/Users/nolson/Documents/ncbi_bac/||')
+	ORG_NAME=$(echo $ORG_NAME_UID | sed 's/_uid.*//')
+	
+	uid=$(echo $ORG_NAME_UID | sed "s/$ORG_NAME\_uid//")
+	
+	echo $ORG_NAME,$uid
+}
+
+function parse_fasta_names(){
+	#take fasta file and returns the gi
+	
+	#get first line in fasta
+	FNA=$(grep '^>' $1)
+	
+	#get gi from fasta line
+	gi=$(echo $FNA | sed 's/.*gi|//')
+	gi=$(echo $gi | sed 's/|.*//')
+	
+	echo $gi
+}
+
+
+#loop through directories
+for DIR in $DB_DIR/*;
+do
+	DIR_PARSE=$(parse_directory_name $DIR)
+	for FASTA in $DIR/*fna;
+		do
+			FASTA_GI=$(parse_fasta_names $FASTA)
+			echo $DIR_PARSE,$FASTA,$FASTA_GI >> $DB_OUT
+		done
+done
+	
--- get_taxa_2.R
+++ get_taxa_2.R
@@ -11,37 +11,53 @@
 ##    project was developed
 ## Output files:csv file with taxa information for provided list of organism names.
 ## 
+## Issue to address genome names with out matches, ideal is to find a more robust method
+## getting taxonomic information/ taxa matching
+##
 #################################################################################################################
 
-library(taxize)
+library(NCBI2R)
 library(reshape2)
 library(plyr)
+library(stringr)
 
+source("file_locations.R")
 sessionInfo()
 
 ##### Input list of organism names used as reference in mapping
 all_bac <- readLines("taxa_names.txt")
 
 ##### Cleaning up names generating list of bacteria names and data frame of names and uid
-uid <- gsub(".*uid","", all_bac)
+uid <- as.numeric(gsub(".*uid","", all_bac))
 all_bac <- gsub("/media/second/DATAFILES/Bacteria/", "", all_bac)
 all_bac <- gsub("_uid.*", "", all_bac)
 all_bac <- gsub("_"," ", all_bac)
 uid_df <- data.frame(uid, full_name = all_bac)
+uid_df <- uid_df[complete.cases(uid_df),]
+
+
+GetTaxInfo(taxids=uid_df$uid[1])$lineage
+
+taxa_df <- data.frame()
+for(i in 1:length(uid)/200){
+  taxa_df <- rbind(taxa_df, GetTaxInfo(taxids=uid[1:200*i])$lineage)
+}
+
 
 ##### Gets taxonomic classification from NCBI database using the taxize package
 taxa_list<- list()
-taxa_list <- classification(unique(all_bac), db = 'ncbi')
+taxa_list <- classification(unique(uid), db = 'ncbi')
 
 
 ### reformat taxize output to dataframe and write to file as csv
 taxa_df <- ldply(taxa_list, data.frame)
-#limit the taxnomic ranks included to eliminat overlap where some levels have multiple values
-taxa_df <- taxa_df[taxa_df$Rank %in% c("phylum","class","order","family","genus","species"),1:3]
+#limit the taxonomic ranks included to eliminat overlap where some levels have multiple values
+taxa_df <- subset(taxa_df, select = c(.id, name, rank))
+taxa_df <- taxa_df[taxa_df$rank %in% c("phylum","class","order","family","genus","species"),1:3]
 
 #note that organisms where to taxonomic classification information is obtained are not included 
 #in the resulting dataframe, this could be an issue downstream
-taxa_df <- plyr::rename(taxa_df, replace = c(".id". "full_name"))
-taxa <- dcast(taxa_df, full_name~Rank, value.var = "ScientificName")
-taxa <- merge(taxa, uid_df, union("all_bac"), all.y = TRUE)
-write.csv(taxa, str_c(working_directory, "/ref_taxa.csv", sep = "")
+taxa_df <- rename(taxa_df, replace = c(".id"= "full_name"))
+taxa <- dcast(taxa_df, full_name~rank, value.var = "name")
+taxa_uid <- join(x = taxa, y = uid_df, type = "right")
+write.csv(taxa_uid, str_c(working_directory, "/ref_taxa.csv", sep = ""))
