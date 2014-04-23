55d4549dd0a3ed3e779cda586e65d1c544d6465b
Nate Olson <nathandavidolson@gmail.com>
2014-04-23 18:59:14 -0400

    prune to remove csv files

--- match_tid.R
+++ match_tid.R
@@ -1,6 +1,50 @@
-### function for matching taxonomy
-match_tid <- function(tid1, tid2){
-  require(NCBI2R)
-  common <- CompareTaxInfo(id1=tid1, id2=tid2)$common
-  return(common[common$line.rank %in% c("kingdom","phylum","class","order","family","genus","species"),])
+### functions for matching taxonomy
+## using the pyphy.py code
+require(rPython)
+python.load("pyphy.py",get.exception=TRUE)
+
+GetTaxInfoLocal <- function(ids){
+  #gets the tid lineage for the specified taxid
+  tid <- python.call("getPathByTaxid", ids)
+  rank <- c()
+  name <- c()
+  for(i in tid){
+    rank <- c(rank,python.call("getRankByTaxid", i))
+    name <- c(name,python.call("getNameByTaxid", i))
+  }
+  lineage <- data.frame(tid,rank,name)
+  lineage <- lineage[lineage$rank %in% c("kingdom","phylum","class","order","family","genus","species"),]
+  return(lineage)
+}
+
+CompareTaxInfoLocal <- function(id1, id2){
+    require(plyr)
+    if (length(id1) != 1 | length(id2) != 1) 
+      stop("Error: One item sep by columns please.")
+    g1 <- GetTaxInfoLocal(id1)
+    g2 <- GetTaxInfoLocal(id2)
+    common <- join(x=g1, y=g2, by= "tid", type="inner")
+    return(common)
+}
+
+GetTaxid <- function(id, type){
+  if (type == 'uid'){
+    return(python.call("getTaxidByUid", id))
+  }else if (type == 'gi'){
+    return(python.call("getTaxidByGi", id))
+  } else {
+    stop("Error: Id number must be specified at 'gi' or 'uid'.")    
+  }
+}
+
+GetMatchLevel <- function(id1, id2){
+  matches <- CompareTaxInfoLocal(id1, id2)
+  
+  #all pairs share this same match - not sure where the error is ...
+  matches <- matches[matches$tid != 131567,]
+  for(level in rev(c("domain","kingdom","phylum","class","order","family","genus","species")))
+    if(level %in% matches$rank){
+      return(level)
+  }
+  return("no match")
 }
--- sim_figures.R
+++ sim_figures.R
@@ -2,5 +2,8 @@ library(ggplot2)
 
 sim_matches <- read.csv("sim_path_matches.csv")
 
-ggplot(sim_matches) + geom_bar(aes(x = as.character(org1), fill = org1_match)) + facet_wrap(~prop1)
-ggplot(sim_matches) + geom_bar(aes(x = as.character(org2), fill = org2_match))
+ggplot(sim_matches) + geom_bar(aes(x = as.character(prop1), 
+                                   fill = org1_match), position = "fill") + 
+  facet_wrap(~org1)
+ggplot(sim_matches) + geom_bar(aes(x = as.character(org2), 
+                                   fill = org2_match),position = "fill")
