4aebc44c741b0e0f9cb0562c0ce4aa058c20723f
Nate Olson <nathandavidolson@gmail.com>
2014-04-26 16:08:07 -0400

    fixing bugs in simulated data gathering and matching

--- file_locations.R
+++ file_locations.R
@@ -1,8 +1,12 @@
 # folder locations
 library(stringr)
-working_directory <- "/media/nolson/second/mirror/purity_study/pathoscope_pure_id"
+purity_directory <- "/media/nolson/second/mirror/purity_study"
+
 if(grepl("apple", sessionInfo()$R.version$platform)){
-  working_directory <- "~/Documents/mirror/purity_study/pathoscope_pure_id"
+  purity_directory <- "~/Documents/mirror/purity_study"
 }
-real_source_directory <- str_c(working_directory, "/sam-report/", sep="")
-sim_source_directory <- str_c(working_directory, "/sam-report/2013_11_23/", sep="")
+working_directory <- str_c(purity_directory, "/pathoscope_pure_id", sep = "")
+results_directory <- str_c(purity_directory, "/results")
+#real_source_directory <- str_c(working_directory, "/sam-report", sep="")
+sim_source_directory <- str_c(results_directory, "/2013_12_04", sep="")
+path_results_directory <- str_c(working_directory,"/results", sep = "")
--- sim_figures.R
+++ sim_figures.R
@@ -1,9 +1,23 @@
 library(ggplot2)
 
-sim_matches <- read.csv("sim_path_matches.csv")
+sim_matches <- read.csv("results//sim_contam_matches.csv", stringsAsFactor = F)
 
-ggplot(sim_matches) + geom_bar(aes(x = as.character(prop1), 
+#figures
+ggplot(sim_matches) + geom_bar(aes(x = as.factor(prop1), 
                                    fill = org1_match), position = "fill") + 
-  facet_wrap(~org1)
-ggplot(sim_matches) + geom_bar(aes(x = as.character(org2), 
-                                   fill = org2_match),position = "fill")
+  facet_wrap(~org1)+ 
+  theme(axis.text.x=element_text(angle=-90))
+
+ggplot(sim_matches) + geom_bar(aes(x = as.factor(prop2), 
+                                   fill = org2_match),position = "fill") + 
+  facet_wrap(~org1)+ 
+  theme(axis.text.x=element_text(angle=-90))
+
+ggplot(sim_matches) + geom_bar(aes(x = as.factor(org2), 
+                                   fill = match), position = "fill") + 
+  facet_grid(size~org1)+ 
+  theme(axis.text.x=element_text(angle=-90))
+
+ggplot(sim_matches) + 
+  geom_bar(aes(x = match, fill = as.character(size)),position = "dodge")  + 
+  theme(axis.text.x=element_text(angle=-90))
