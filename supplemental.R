a2c1a0fff831708bcb3f25a3635ee4843ec753bb
Nate Olson <nathandavidolson@gmail.com>
2014-05-05 12:58:51 -0400

    paper revisions

--- supplemental.R
+++ supplemental.R
@@ -1,6 +1,6 @@
 #supplement materials
 make supplemental material table with complete list of single organisms -> csv unless other supplemental material will be provided
-
+include org name, taxid, genbank seq ref
 # sensitivity
 - show the counts and final guess distributions for the different taxonomic levels
 
