6c549e8d8cbb884d130d402241e6e71de7ccdd19
Nate Olson <nathandavidolson@gmail.com>
2014-05-05 06:39:01 -0400

    paper outline revisions

---
+++ supplemental.R
@@ -0,0 +1,8 @@
+#supplement materials
+make supplemental material table with complete list of single organisms -> csv unless other supplemental material will be provided
+
+# sensitivity
+- show the counts and final guess distributions for the different taxonomic levels
+
+# specificity 
+- show the genus level plots for the 75 bp reads
