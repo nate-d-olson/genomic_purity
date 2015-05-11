a0c9634601bdfe6234f6cdca25e6fa708ffa4911
nate-d-olson <nolson@nist.gov>
2015-05-11 18:33:34 -0400

    single isolate purity with config file

--- pathoscope_commands.py
+++ pathoscope_commands.py
@@ -3,78 +3,126 @@
 ## functions from pepr pathoscope_commands.py 
 
 from ruffus import *
+
+import os
 import sys
 import time
 import subprocess
-from re import sub
-# starting_files = ["TSRR1979039_1.fastq", "TSRR1979039_2.fastq"] #'SRR2002412.fastq'] #
 
-## working with file pairs
-## template from http://www.ruffus.org.uk/tutorials/new_tutorial/inputs_code.html example 4
-# @transform( source_files,
-#             formatter(".cpp$"),
-#                         # corresponding header for each source file
-#             add_inputs("{basename[0]}.h",
-#                        # add header to the input of every job
-#                        "universal.h"),
-#             "{basename[0]}.o")
-
-
-## need to use new object oriented syntax
-## - minimizes need for modifying existing pipeline functions
-
-
-# @transform(starting_files, suffix(".fastq"),"_qc.fq.gz", '../test_files','4')
-def pathoqc_command(input, output, out_dir, proc):
-    # ## log file stores standard out
-    # log_file = open(log_dir + "/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
-    # stderr_file = open(log_dir + "/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
-    
-    qc_cmd = ["python","/PathoScope/pathoqc_v0.1.2/pathoqc.py"]
-    
-    # if fastq2 != None:
-    #     pathoqc_command = pathoqc_command #+ ['-1',fastq1, '-2',fastq2,'-s',plat,'-p',str(thread_num),'-o',out_dir]
-    # else:
-    qc_cmd = qc_cmd + ['-1',input, '-o', out_dir,'-p', proc]
-    # subprocess.call(pathoqc_command) #, stdout=log_file, stderr=stderr_file)      
-    subprocess.call(qc_cmd)
-
-
-# @transform(pathoqc_command, suffix("_qc.fq.gz"), "-appendAlign.sam", 
-# 			# using MG002 pacbio as ref - smaller, quicker!
-# 			'CFSAN030013.fasta',
-# 			'../test_files',
-# 			# '/current_projects/micro_rm/micro_rm_dev/utilities/patho_utils/micro_rm_patho_db_ti_0.fa', 
-# 			# '/current_projects/micro_rm/micro_rm_dev/utilities/patho_utils/',
-# 			'../test_files',"T")
-def pathomap_command(input, output, ref_path, index_dir, exptag):
+def pathoqc_command(input, output, pathoqc_config):
+    print input
+    print output
     ## log file stores standard out
-    # log_file = open(log_dir + "/pathomap"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
-    # stderr_file = open(log_dir + "/pathomap"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+    out_dir = os.path.dirname(input[0])
+    log_file = open(out_dir + "/logs/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+    stderr_file = open(out_dir + "/logs/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+    
+
+    pathoqc_command = ["python","/PathoScope/pathoqc_v0.1.2/pathoqc.py"]
+    
+    pathoqc_command = pathoqc_command + ['-1',input[0], '-2',input[1],
+                                        '-s',pathoqc_config['plat'],'-p',pathoqc_config['thread_num'], '-o', out_dir]
+
+    subprocess.call(pathoqc_command, stdout=log_file, stderr=stderr_file)        
+
+def pathomap_command(input, output, config): # ref_path, index_dir, fastq1, log_dir, out_dir, out_sam, exptag):
+    ## log file stores standard out
+    out_dir = os.path.dirname(input[0])
+    log_file = open(out_dir + "/logs/pathomap"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+    stderr_file = open(out_dir + "/logs/pathomap"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
 
     ## pathoscope command root
-    pathomap_command = ["python", "/PathoScope/pathoscope/pathoscope.py",
-    					'--verbose','MAP', '-targetRefFiles',ref_path,
-                        '-indexDir',index_dir, '-outAlign', output, '-expTag', input]
+    pathomap_command = ["python", "/PathoScope/pathoscope/pathoscope.py",'--verbose','MAP', '-targetRefFiles',config['ref'],\
+                        '-indexDir',config['index_dir'],'-outDir', out_dir,'-outAlign', output]#, '-expTag', '']
     
-    # if fastq2:
-    #     pathomap_command = pathomap_command + ['-1',fastq1, '-2',fastq2]
-    # else:
-    pathomap_command = pathomap_command + ['-U',input]
-    subprocess.call(pathomap_command)#, stdout=log_file, stderr=stderr_file)
+    pathomap_command = pathomap_command + ['-1',input[0], '-2',input[1]]
 
-# @transform(pathomap_command, suffix("-appendAlign.sam"), "-sam-report.tsv", '../test_files','T')
-def pathoid_command(input, output, out_dir, exptag):
+    subprocess.call(pathomap_command, stdout=log_file, stderr=stderr_file)
+
+def pathoid_command(input, output):
     # command for running pathoid
-
-    # ## log file stores standard out
-    # log_file = open(log_dir + "/pathoid"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
-    # stderr_file = open(log_dir + "/pathoid"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+    out_dir = os.path.dirname(input)
+    log_file = open(out_dir + "/logs/pathoid"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+    stderr_file = open(out_dir + "/logs/pathoid"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
     
     ## pathoscope command root
-    pathoid_command = ["python","/PathoScope/pathoscope/pathoscope.py",'--verbose','ID', 
-    					'-alignFile',input,'-fileType',
-                       'sam','--outMatrix','-expTag', input]
-    subprocess.call(pathoid_command)#, stdout=log_file,stderr=stderr_file)
+    pathoid_command = ["python","/PathoScope/pathoscope/pathoscope.py",'--verbose','ID', '-alignFile',input,'-fileType',
+                       'sam','-outDir',out_dir,'--outMatrix']#,'-expTag', exptag]
+    subprocess.call(pathoid_command, stdout=log_file,stderr=stderr_file)
 
-# pipeline_run()
+
+# import sys
+# import time
+# import subprocess
+# from re import sub
+# # starting_files = ["TSRR1979039_1.fastq", "TSRR1979039_2.fastq"] #'SRR2002412.fastq'] #
+
+# ## working with file pairs
+# ## template from http://www.ruffus.org.uk/tutorials/new_tutorial/inputs_code.html example 4
+# # @transform( source_files,
+# #             formatter(".cpp$"),
+# #                         # corresponding header for each source file
+# #             add_inputs("{basename[0]}.h",
+# #                        # add header to the input of every job
+# #                        "universal.h"),
+# #             "{basename[0]}.o")
+
+
+# ## need to use new object oriented syntax
+# ## - minimizes need for modifying existing pipeline functions
+
+
+# # @transform(starting_files, suffix(".fastq"),"_qc.fq.gz", '../test_files','4')
+# def pathoqc_command(input, output, out_dir, proc):
+#     # ## log file stores standard out
+#     # log_file = open(log_dir + "/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+#     # stderr_file = open(log_dir + "/pathoqc"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+    
+#     qc_cmd = ["python","/PathoScope/pathoqc_v0.1.2/pathoqc.py"]
+    
+#     # if fastq2 != None:
+#     #     pathoqc_command = pathoqc_command #+ ['-1',fastq1, '-2',fastq2,'-s',plat,'-p',str(thread_num),'-o',out_dir]
+#     # else:
+#     qc_cmd = qc_cmd + ['-1',input, '-o', out_dir,'-p', proc]
+#     # subprocess.call(pathoqc_command) #, stdout=log_file, stderr=stderr_file)      
+#     subprocess.call(qc_cmd)
+
+
+# # @transform(pathoqc_command, suffix("_qc.fq.gz"), "-appendAlign.sam", 
+# # 			# using MG002 pacbio as ref - smaller, quicker!
+# # 			'CFSAN030013.fasta',
+# # 			'../test_files',
+# # 			# '/current_projects/micro_rm/micro_rm_dev/utilities/patho_utils/micro_rm_patho_db_ti_0.fa', 
+# # 			# '/current_projects/micro_rm/micro_rm_dev/utilities/patho_utils/',
+# # 			'../test_files',"T")
+# def pathomap_command(input, output, ref_path, index_dir, exptag):
+#     ## log file stores standard out
+#     # log_file = open(log_dir + "/pathomap"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+#     # stderr_file = open(log_dir + "/pathomap"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+
+#     ## pathoscope command root
+#     pathomap_command = ["python", "/PathoScope/pathoscope/pathoscope.py",
+#     					'--verbose','MAP', '-targetRefFiles',ref_path,
+#                         '-indexDir',index_dir, '-outAlign', output, '-expTag', input]
+    
+#     # if fastq2:
+#     #     pathomap_command = pathomap_command + ['-1',fastq1, '-2',fastq2]
+#     # else:
+#     pathomap_command = pathomap_command + ['-U',input]
+#     subprocess.call(pathomap_command)#, stdout=log_file, stderr=stderr_file)
+
+# # @transform(pathomap_command, suffix("-appendAlign.sam"), "-sam-report.tsv", '../test_files','T')
+# def pathoid_command(input, output, out_dir, exptag):
+#     # command for running pathoid
+
+#     # ## log file stores standard out
+#     # log_file = open(log_dir + "/pathoid"+time.strftime("-%Y-%m-%d-%H-%M-%S.log"),'w')
+#     # stderr_file = open(log_dir + "/pathoid"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
+    
+#     ## pathoscope command root
+#     pathoid_command = ["python","/PathoScope/pathoscope/pathoscope.py",'--verbose','ID', 
+#     					'-alignFile',input,'-fileType',
+#                        'sam','--outMatrix','-expTag', input]
+#     subprocess.call(pathoid_command)#, stdout=log_file,stderr=stderr_file)
+
+# # pipeline_run()
---
+++ sipp.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+import yaml
+"""
+        Simulated isolate purity pipeline
+        Author: Nathan Olson
+        Organization: NIST
+        Email: nolson@nist.gov
+        Date: 5/11/2015
+        Version: 0.0.0.9000
+
+        Code based on Ruffus OO syntax worked example
+        http://www.ruffus.org.uk/tutorials/new_syntax_worked_example.html
+"""
+import os
+import sys
+from ruffus import *
+from pathoscope_commands import *
+from ngs_simulate_commands import *
+
+#import unittest ## need to add tests ....
+
+###############################################################################
+##
+## Functions for generating skeleton pipe
+##
+###############################################################################
+
+import shutil
+
+
+def touch (outfile):
+    with open(outfile, "w"):
+        pass
+
+# def get_genome_fasta(input,output,extras):
+# 	touch(output)
+
+# def simulate_miseq(input,output,extras):
+# 	for i in output:
+# 		touch(i)
+
+# def pathoqc_command(input,output, extras):
+# 	for i in output:
+# 		touch(i)
+
+# def pathomap_command(input, output,extras):
+# 	touch(output)
+
+# def pathoid_command(input,output,extras):
+# 	touch(output)
+
+
+tempdir = "tempdir/"
+def task_originate(o):
+    """
+    Makes new files
+    """
+    touch("start_" + o)
+
+###############################################################################
+##
+## Skeleton pipe
+##
+###############################################################################
+def make_sipp(org_list, config, pipeline_name = 'sipp'):
+	sip_pipe = Pipeline(pipeline_name)
+
+	sip_pipe.originate(task_originate, org_list) 
+	
+
+	sip_pipe.transform(get_genome_fasta,org_list,formatter("(?P<org>\w+)_uid(?P<uid>\w+)"),
+						"{org[0]}_uid{uid[0]}/{uid[0]}_.fasta", config['genome_dir'])\
+        				.follows(mkdir(org_list), mkdir([i + "/logs/" for i in org_list]), 
+        							mkdir([i + "/tmp/" for i in org_list]))
+
+	sip_pipe.transform(	simulate_miseq, output_from("get_genome_fasta"),
+						suffix(".fasta"),["1.fq","2.fq"])#, extras = ['a'])
+
+	sip_pipe.transform(pathoqc_command,output_from("simulate_miseq"),
+						regex(r"_[12].fq"), ["_1_qc.fq.gz", "_2_qc.fq.gz"],config['pathoqc'])
+
+	sip_pipe.transform(	pathomap_command,output_from("pathoqc_command"),
+					formatter("(?P<uid>\w+)_[12]_qc_fq.gz"), "pathomap-config['ref_root'].sam", config['pathomap']) #may need to change output suffix to -appendAlign.sam, for larger ref files
+#regex(r"_[12]_qc.fq.gz")
+	sip_pipe.transform(pathoid_command,output_from("pathomap_command"),
+						suffix(".sam"),"-sam-report.tsv")#may need to change input suffix to -appendAlign.sam, for larger ref files
+
+	sip_pipe.set_head_tasks([sip_pipe[task_originate]])
+
+	return sip_pipe
+# test_org_list = ["/current_projects/genomic_purity/test_files/Acaryochloris_marina_MBIC11017_uid12997"]
+# pipeline1a = make_sipp(org_list = test_org_list)
+
+###############################################################################
+##
+## Commandline iterface
+##
+###############################################################################
+
+import ruffus.cmdline as cmdline
+parser = cmdline.get_argparse(description='Pipelines for genomic contaminant study',
+                              version = "genomic_contamination_study_pipe.py v. 0.0.0.9000")
+
+# parser.add_argument('--pipeline', "-p", 
+# 					type=str, 
+# 					choices = ['sim_pure', 'sim_contam','real_pure','test_pipe'],
+#                     help="Defining which pipeline to run")
+
+parser.add_argument('--config_file', "-cf", 
+					type=str,
+					#metavar="config_file",
+					help="yaml file with pipeline parameters")
+
+options = parser.parse_args()
+
+
+
+## standard python logger which can be synchronised across concurrent Ruffus tasks
+## define logging output with --log_file  log_file_name
+logger, logger_mutex = cmdline.setup_logging (__name__, options.log_file, options.verbose)
+
+
+# if we are printing only
+if  not options.just_print and \
+    not options.flowchart and \
+    not options.touch_files_only:
+
+    config_file= file(options.config_file, 'r')
+    config = yaml.load(config_file)
+
+    print config
+    pipeline1a = make_sipp(org_list = config['org_list'], config = config)
+    cmdline.run (options, logger = logger)
+    sys.exit()
