61eb2b1136ae5b05056e0622568689900b7eed15
nate-d-olson <nolson@nist.gov>
2015-06-09 10:54:51 -0400

    cleaning up

--- purity_study_v1/supplemental.R
+++
@@ -1,8 +0,0 @@
-#supplement materials
-make supplemental material table with complete list of single organisms -> csv unless other supplemental material will be provided
-include org name, taxid, genbank seq ref
-# sensitivity
-- show the counts and final guess distributions for the different taxonomic levels
-
-# specificity 
-- show the genus level plots for the 75 bp reads
