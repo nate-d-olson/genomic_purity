46b8270c3ff394955ae412121ac7464806114d6c
Nathan Olson <nolson@nist.gov>
2015-06-29 18:19:31 -0400

    parallelization

--- compress-fastq-fasta.sh
+++ compress-fastq-fasta.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sh
 ## compressing fastq files
-for f in */*fq */*fasta;
+#for f in */*fq */*fasta;
+for f in Clostridium*/*fq Clostridium*/*fasta; #Clostridium specific
 do
 # adding & to loop to run the command in the background - cheap parallelization
 	gzip $f &
--- sam-to-bam.sh
+++ sam-to-bam.sh
@@ -2,8 +2,30 @@
 ## compressing fastq files
 #for sam in Escherichia*/*sam;
 # for sam in Listeria*/*sam
-for sam in Bacillus*/*sam
+#for sam in Bacillus*/*sam
+for sam in Clostridium*/*sam
 do
 	samtools view -bS -@ 8 -o $sam.bam $sam
 	rm $sam
 done
+
+## adding parallization
+# maxjobs=$( ls -d /sys/devices/system/cpu/cpu[[:digit:]]* | wc -w )
+# parallelize () {
+#         while [ $# -gt 0 ] ; do
+#                 jobcnt=(`jobs -p`)
+#                 if [ ${#jobcnt[@]} -lt $maxjobs ] ; then
+#                         compress_sam $1 &
+#                         shift  
+#                 fi
+#         done
+#         wait
+# }
+
+
+# compress_sam () {
+# 	samtools view -bS -@ 8 -o $1.bam $1
+# 	rm $1
+# }
+
+# parallelize Clostridium*/*sam
